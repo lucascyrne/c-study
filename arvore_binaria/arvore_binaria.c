b6614dfada726f72b102e9a3c8b7c13cb37827f4
Lucas <lcf@cesar.school>
2019-09-30 09:55:00 -0300

    new exercise: arvore_binaria

--- arvore_binaria/arvore_binaria.c
+++ arvore_binaria/arvore_binaria.c
@@ -1,85 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-typedef struct Folha {
-        struct Folha *direita;
-        struct Folha *esquerda;
-        struct Folha *pai;        
+struct leaf {
         int data;
-        int altura;
-}Leaf;
+        struct leaf *left;
+        struct leaf *right;
+        struct leaf *pai;
+};
 
+struct leaf* newLeaf(int data) {
+        struct leaf* newLeaf = (struct leaf *)malloc(sizeof(struct leaf));
+        newLeaf->data = data;
+        newLeaf->left = NULL;
+        newLeaf->right = NULL;
 
-void *createTree(int new_data, Leaf *esq, Leaf *dir, Leaf *Raiz);
-void *growLeaves(int new_data, Leaf *Tree);
-int dataget();
+        return newLeaf;
+};
 
-void main() { 
-        int new_data, resp;
-        Leaf *esq;
-        Leaf *dir;
-        Leaf *Raiz, *Tree;
+struct leaf *tree(int data, struct leaf* esq, struct leaf* dir) {
+        struct leaf *raiz;
 
-        new_data = dataget();
-        if(new_data > 100) {
-                printf("Wrong value, choose another");
-                return;
+        raiz = (struct leaf *)malloc(sizeof(struct leaf));
+        raiz->data = data;
+        raiz->left = esq;
+        raiz->right = dir;
+        esq->pai = raiz;
+        dir->pai = raiz;
 
-        } else {
-                createTree(new_data, esq, dir, Raiz);
-        }
-        printf("Deseja continuar? S / N");
-        scanf("%i", &resp);
-        if(resp == 0) {
-                printf("Fim da árvore");
-                return;
-        } else {
-                new_data = dataget();
-                growLeaves(new_data, Tree);
-        }
-
-        
-        return;
+        return raiz;
 }
 
-int dataget() {
-        int new_data;
+struct leaf *insert(struct leaf *raiz, int data) {
 
-        printf("Create new tree?");
-        printf("\nPick a value < 100: ");
-        scanf("%i", &new_data);
-
-        return new_data;
-}
-
-void *createTree(int new_data, Leaf *esq, Leaf *dir, Leaf *Raiz) {
-
-        Raiz = (Leaf *)malloc(sizeof(Leaf));
-        Raiz->data = new_data;
-        esq = (Leaf *)malloc(sizeof(Leaf));
-        Raiz->esquerda = esq;
-        dir = (Leaf *)malloc(sizeof(Leaf));
-        Raiz->direita = dir;
-        esq->pai = Raiz;
-        dir->pai = Raiz;
-
-        return(Raiz);
-}
-
-void *growLeaves(int new_data, Leaf *Tree) {
-
-        if(Tree == NULL) {
-                Tree = createTree(new_data, NULL, NULL, NULL);
+        if(raiz == NULL) return newLeaf(data);
+        if(data < raiz->data) {
+                raiz->left = insert(raiz->left, data);
+                raiz->left->pai = raiz;
         } else {
-                if(new_data <= Tree->data) {
-                        Tree->esquerda = growLeaves(new_data, Tree->esquerda);
-                        Tree->esquerda->pai = Tree;
-                } else {
-                        Tree->direita = growLeaves(new_data, Tree->direita);
-                        Tree->direita->pai = Tree;
-                }
+                raiz->right = insert(raiz->right, data);
+                raiz->right->pai = raiz;
         }
 
-        return(Tree);
+        return raiz;
+
 }
+
+struct leaf *search(struct leaf *raiz, int data) {
+        if(raiz == NULL || raiz->data == data) {
+                return raiz;
+        }
+
+        if(data < raiz->data) {
+                return search(raiz->left, data);
+        } else {
+                return search(raiz->right, data);
+        }
+}
+
+int preOrd(struct leaf *raiz);
+
+
+
+int main() {
+
+        struct leaf *raiz = NULL;
+        raiz = insert(raiz, 50);
+        insert(raiz, 30);
+        insert(raiz, 20);
+        insert(raiz, 70);
+
+        preOrd(raiz);
+
+        getchar();
+        return 0;
+
+}
+
+int preOrd(struct leaf *raiz) {
+        if(raiz != NULL) {
+                printf("%i ", raiz->data);
+                preOrd(raiz->left);
+                preOrd(raiz->right);
+        }
+};
+
